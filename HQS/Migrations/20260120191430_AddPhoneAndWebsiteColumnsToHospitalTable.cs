10bae60b68d928187b71682d0e6528ac266f19a2
Sirijan <er.sirijanbhalla@gmail.com>
2026-01-28 14:17:07 +0530

    all migrations so far

---
+++ HQS/Migrations/20260120191430_AddPhoneAndWebsiteColumnsToHospitalTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HQS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhoneAndWebsiteColumnsToHospitalTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Hospitals",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Website",
+                table: "Hospitals",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Hospitals");
+
+            migrationBuilder.DropColumn(
+                name: "Website",
+                table: "Hospitals");
+        }
+    }
+}
---
+++ HQS/Migrations/20260121024539_AddWheelchairColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HQS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWheelchairColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWheelchairAccessible",
+                table: "Hospitals",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsWheelchairAccessible",
+                table: "Hospitals");
+        }
+    }
+}
